a9608e453a76684935bfdcf81409f4bc4c18c1e9
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-02-21 23:44:58 -0500

    deleting unnecessary files

---
+++ src/AgileWebPro.jsx
@@ -0,0 +1,7 @@
+export const AgileWebPro = () => {
+  return (
+    <div>
+      <h1>AgileWebPro</h1>
+    </div>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { AgileWebPro } from './AgileWebPro'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AgileWebPro />
   </React.StrictMode>,
 )
