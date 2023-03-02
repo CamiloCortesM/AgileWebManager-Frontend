614a3fd771d01a2957c68d55c50380f586d5ed6e
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-03-02 13:40:41 -0500

    remove folder assets

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,7 +1,28 @@
+import "../styles.css";
+
 export const LoginPage = () => {
   return (
-    <div>
-      <h1>LoginPage</h1>
+    <div className="Login">
+      <div className="login__header">
+        <h2 className="login__header__title">Login to Your Account</h2>
+        <p className="login__header__description">
+          login using email and password
+        </p>
+      </div>
+      <div className="login__body">
+        <div className="login__body__email">
+          <input placeholder="email" className="login__input__email input" />
+        </div>
+        <div className="login__body__password">
+          <input
+            placeholder="password"
+            className="login__input__password input"
+          />
+        </div>
+      </div>
+      <div className="login__footer">
+        <button className="login__footer__bottom">Login</button>
+      </div>
     </div>
   );
 };
