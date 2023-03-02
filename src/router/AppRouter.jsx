787eb484e9122709a91bd61292702440143b634a
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-03-02 10:48:54 -0500

    import font and var colors declarations

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,7 +3,7 @@ import { LoginPage } from "../auth/pages/LoginPage";
 import { TablePage } from "../table/pages/TablePage";
 
 export const AppRouter = () => {
-  // const state = "not-authenticated" 
+  const state = "not-authenticated" 
   return (
     <Routes>
       {state === "not-authenticated" ? (
