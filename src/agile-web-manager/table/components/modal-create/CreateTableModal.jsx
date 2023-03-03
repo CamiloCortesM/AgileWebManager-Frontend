018061582640f5085d2d53c3c7b0e4a3fb81d0c7
DANIELSSF <daniel.santiago730@gmail.com>
2023-03-03 13:42:49 -0500

    Modal with the form to create tables

---
+++ src/agile-web-manager/table/components/modal-create/CreateTableModal.jsx
@@ -0,0 +1,57 @@
+import './style.css';
+
+
+export const CreateTableModal = ({ toggleModal }) => {
+
+  const handleCloseModal = () => {
+    toggleModal();
+  }
+
+
+  return (
+    <div className="create-table-modal">
+
+      <div className="create-table-modal__container">
+
+        <div className="create-table-modal__title">
+          <span onClick={handleCloseModal}>
+            <img
+              className="create-table-modal__title__x"
+              src="public/x.svg" alt="x.svg"
+            />
+          </span>
+          <h2 className="create-table-modal__title__h2">Create Table</h2>
+        </div>
+
+        <form
+          autoComplete="off"
+          className="create-table-modal__form"
+        >
+          <h3 className="create-table-modal__subtitle">Table name</h3>
+          <input
+            className="create-table-modal__input"
+            placeholder="Table name"
+            type="text"
+          />
+          <h3 className="create-table-modal__subtitle">Description</h3>
+          <textarea
+            className="create-table-modal__input create-table-modal__input__textarea"
+            placeholder="Description"
+            type="text"
+          />
+
+          <div className="create-table-modal__buttons">
+            <button
+              className="create-table-modal__button create-table-modal__button__close"
+              onClick={handleCloseModal}
+            >
+              Cancel</button>
+            <button className="create-table-modal__button create-table-modal__button__create">Create</button>
+          </div>
+        </form>
+      </div>
+
+
+    </div>
+  )
+}
