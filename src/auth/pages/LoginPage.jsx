86a317fb57d844053a21dba6422ebe4a113a62af
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-03-02 20:06:15 -0500

    the login structure has been completed

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -9,9 +9,13 @@ export const LoginPage = () => {
           login using email and password
         </p>
       </div>
+      <div className="login__errors">
+        <p></p>
+      </div>
       <div className="login__body">
         <div className="login__body__email login__input">
           <input placeholder="Email" type="email" className="login__input__email input" />
+          <img className="login_body__icon--Email login_body__icon" src="/public/icons/user-svgrepo-com.svg" alt="imagen2" />
         </div>
         <div className="login__body__password login__input">
           <input
@@ -19,6 +23,7 @@ export const LoginPage = () => {
             placeholder="Password"
             className="login__input__password input"
           />
+          <img className="login_body__icon--Email login_body__icon" src="/public/icons/lock-off-svgrepo-com.svg" alt="imagen2" />
         </div>
       </div>
       <div className="login__footer">
