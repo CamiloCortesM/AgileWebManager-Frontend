377c8f1dc9242e114308c19b0dd313c71587bc57
DANIELSSF <daniel.santiago730@gmail.com>
2023-03-04 15:06:33 -0500

    Barrel file

--- src/agile-web-manager/todos/index.js
+++ src/agile-web-manager/todos/index.js
@@ -1,1 +1,7 @@
+export * from './components/add-tarjet/AddTarjet';
+export * from './components/change-todo/ChangeTodo';
+export * from './components/delete-todo/DeleteTodo';
+export * from './components/todo-in-progress/InProgress';
+export * from './components/todo-not-started/NotStarted';
+export * from './components/todo-done/Done';
 export * from './pages/TodoPages';
