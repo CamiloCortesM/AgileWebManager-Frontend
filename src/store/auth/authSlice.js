3b77beaf4ef93a08a362ce6d6376fe00804eb9e4
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-03-04 00:35:51 -0500

    fix errors

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -1,40 +1,48 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const authSlice = createSlice({
-    name:"auth",
-    initialState:{
-        state:"checking", // "not-authenticated" "login-not-athenticated"  "new-user" "authenticated"
-        user:{},
-        errorMessage:undefined
+  name: "auth",
+  initialState: {
+    state: "not-authenticated", // "checking" "login-not-athenticated"  "new-user" "authenticated"
+    user: {},
+    errorMessage: undefined,
+  },
+  reducers: {
+    onChecking: (state) => {
+      state.state = "checking";
+      state.user = {}; 
+      state.errorMessage = undefined;
     },
-    reducers: {
-        onChecking: (state) =>{
-            state.state = "checking";
-            state.user = {},
-            state.errorMessage = undefined;
-        },
-        onLogin: (state, {payload}) =>{
-            state.state = "login-not-athenticated";
-            state.user = payload,
-            state.errorMessage = undefined;
-        },
-        onCreate: (state) =>{
-            state.state = "new-user";
-            state.errorMessage = undefined;
-        },
-        onVerify: (state,{payload}) =>{
-            state.state = "authenticated";
-            state.user = payload,
-            state.errorMessage = undefined;
-        },
-        onLogout: (state,{payload}) =>{
-            state.state = "not-authenticated";
-            state.user = {};
-            state.errorMessage = payload;
-        },
+    onLogin: (state, { payload }) => {
+      state.state = "login-not-athenticated";
+      state.user = payload;
+      state.errorMessage = undefined;
     },
-})
+    onCreate: (state) => {
+      state.state = "new-user";
+      state.errorMessage = undefined;
+    },
+    onVerify: (state, { payload }) => {
+      state.state = "authenticated";
+      state.user = payload; 
+      state.errorMessage = undefined;
+    },
+    onLogout: (state, { payload }) => {
+      state.state = "not-authenticated";
+      state.user = {};
+      state.errorMessage = payload;
+    },
+    clearErrorMessage: (state) => {
+      state.errorMessage = undefined;
+    },
+  },
+});
 
-
-
-export const {onChecking,onCreate,onLogin,onLogout,onVerify} = authSlice.actions
+export const {
+  onChecking,
+  onCreate,
+  onLogin,
+  onLogout,
+  onVerify,
+  clearErrorMessage,
+} = authSlice.actions;
