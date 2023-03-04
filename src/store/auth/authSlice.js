78c077e1dc213519eee363dcd0cbc7dcaf117184
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-03-04 18:58:51 -0500

    checking is initialstate

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const authSlice = createSlice({
   name: "auth",
   initialState: {
-    state: "not-authenticated", // "checking" "login-not-athenticated"  "new-user" "authenticated"
+    state: "checking", // "checking" "login-not-athenticated"  "new-user" "authenticated"
     user: {},
     errorMessage: undefined,
   },
