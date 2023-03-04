a2c251270f8c0498d62eef8e277c8eb983949dc1
DANIELSSF <daniel.santiago730@gmail.com>
2023-03-04 15:02:08 -0500

    added a route for todos

--- src/agile-web-manager/routes/AuthenticateRoutes.jsx
+++ src/agile-web-manager/routes/AuthenticateRoutes.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import { TablePage } from '../table';
+import { TodoPages } from '../todos';
 import { Navbar } from '../ui';
 
 export const AuthenticateRoutes = () => {
@@ -8,6 +9,7 @@ export const AuthenticateRoutes = () => {
       <Navbar />
       <Routes>
         <Route path='/' element={<TablePage />} />
+        <Route path='/todos' element={<TodoPages />} />
       </Routes>
     </>
   )
