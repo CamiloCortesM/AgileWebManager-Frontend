99158b90dacb32239d3f830fd5bd8ab5d1c3116a
DANIELSSF <daniel.santiago730@gmail.com>
2023-03-04 15:19:56 -0500

    List of completed Todos

---
+++ src/agile-web-manager/todos/components/todo-done/Done.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import { AddTarjet, ChangeTodo, DeleteTodo } from '../index';
+import '../style.css';
+
+export const Done = () => {
+    const [addTarjet, setAddTarjet] = useState(false);
+
+    return (
+        <div className="todo">
+            <h2 className="todo__title">todo</h2>
+            <ul className="todo__tarjet__container">
+                <li className="todo__tarjet">
+                    <h3 className="todo__tarjet__subtitle">Ya juegue</h3>
+                    <div className="todo__tarjet__icons">
+                        <img
+                            src="public/icons/text.svg"
+                            className="todo__tarjet__text"
+                            alt="text.svg"
+                        ></img>
+                        <div className="todo__tarjet__comments">
+                            <img
+                                src="public/icons/message.svg"
+                                className="todo__tarjet__message"
+                                alt="message.svg"
+                            ></img>
+                            2
+                        </div>
+                        <div className="todo__tarjet__space"></div>
+                        <ChangeTodo />
+                        <DeleteTodo />
+                    </div>
+                </li>
+            </ul>
+
+            {
+                addTarjet
+                    ?
+                    <>
+                        <AddTarjet />
+                        <div className="todo__add-tarjet__create">
+                            <span
+                                onClick={() => { }}
+                            >
+                                <img
+                                    className="todo__add-tarjet__confirm"
+                                    src="public/icons/check.svg" alt="check.svg"
+                                />
+                            </span>
+                            <span
+                                onClick={() => setAddTarjet(false)}
+                            >
+                                <img
+                                    className="todo__add-tarjet__cancel"
+                                    src="public/icons/x.svg" alt="x.svg"
+                                />
+                            </span>
+                        </div>
+                    </>
+                    : <span
+                        onClick={() => setAddTarjet(true)}
+                        className="todo__add-tarjet"
+                    >
+                        <img
+                            className="todo__add-tarjet__add"
+                            src="public/icons/plus.svg"
+                            alt="plus.svg"
+                        />
+                        Add tarjet</span>
+            }
+        </div>
+    )
+}
