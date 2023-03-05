5e74166a4287c635c9a484af9b6957a62268ae58
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-03-04 23:50:19 -0500

    style correction and implementation of user information in the navbar

--- src/agile-web-manager/ui/componets/Navbar.jsx
+++ src/agile-web-manager/ui/componets/Navbar.jsx
@@ -1,40 +1,58 @@
-import { useState } from 'react';
-import './style.css';
-
+import { useState } from "react";
+import { getLetter } from "../../../helpers/getLetter";
+import { useAuthStore } from "../../../hooks/useAuthStore";
+import "./style.css";
 
 export const Navbar = () => {
-
   const [isActive, setIsActive] = useState(false);
 
+  const { user, startLogout } = useAuthStore();
+
   const toggleButton = () => {
-    setIsActive(!isActive)
-  }
+    setIsActive(!isActive);
+  };
+
+  const onLogout = () => {
+    startLogout();
+  };
 
   return (
     <nav className="navbar">
-
       <div className="navbar__container">
         <h2 className="navbar__title">Agile Web Manager</h2>
         <h2 className="navbar__tables">Tables</h2>
 
         <div className="navbar__space"></div>
-        <button className="navbar__account" onClick={toggleButton}>D</button>
+        <button className="navbar__account" onClick={toggleButton}>
+          {getLetter(user.name)}
+        </button>
 
-        <div className={`navbar__menu ${isActive ? "navbar__menu--on" : "navbar__menu--off"}`}>
+        <div
+          className={`navbar__menu ${
+            isActive ? "navbar__menu--on" : "navbar__menu--off"
+          }`}
+        >
           <p className="navbar__menu__p">Account</p>
           <div className="navbar__menu__list info">
-            <p className="navbar__menu__list--name">D</p>
+            <p className="navbar__menu__list--name">{getLetter(user.name)}</p>
             <div className="navbar__menu__list--container">
-              <p className="navbar__menu__list--name-full">DANIEL SANTIAGO SILVA FONSECA</p>
-              <p className="navbar__menu__list--email">daniel@google.com</p>
+              <p className="navbar__menu__list--name-full">
+                {user.name.toUpperCase()}
+              </p>
+              <p className="navbar__menu__list--email">{user.email}</p>
             </div>
           </div>
-          <button className="navbar__menu__list navbar__menu__confi">Manage Account</button>
-          <button className="navbar__menu__list navbar__menu__close">Logout</button>
+          <button className="navbar__menu__list navbar__menu__confi">
+            Manage Account
+          </button>
+          <button
+            className="navbar__menu__list navbar__menu__close"
+            onClick={onLogout}
+          >
+            Logout
+          </button>
         </div>
-
       </div>
-
     </nav>
   );
 };
