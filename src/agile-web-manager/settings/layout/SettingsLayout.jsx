573e383591bedffbe94e97b405e912ec5fbe8847
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-03-06 01:29:02 -0500

    actions have been added in the configuration layout

--- src/agile-web-manager/settings/layout/SettingsLayout.jsx
+++ src/agile-web-manager/settings/layout/SettingsLayout.jsx
@@ -1,5 +1,16 @@
+import { NavLink, useLocation } from "react-router-dom";
+import { useAuthStore } from "../../../hooks/useAuthStore";
 import "./styles.css";
 export const SettingsLayout = ({ children }) => {
+  const { user, startLogout } = useAuthStore();
+  const { role } = user;
+
+  const location = useLocation();
+  const { pathname } = location;
+
+  const onLogout = () => {
+    startLogout();
+  };
   return (
     <div className="settings">
       <nav className="settings__sidebar">
@@ -9,11 +20,13 @@ export const SettingsLayout = ({ children }) => {
             src="/public/icons/cat-halloween-kitty-svgrepo-com.svg"
             alt="cat"
           />
+          <NavLink to="/" className="navlink">
           <img
             className="settings__sidebar--icon"
             src="/public/icons//back-svgrepo-com.svg"
             alt="back"
           />
+          </NavLink>
         </div>
         <div className="settings__sidebar__nav">
           <div className="settings__sidebar__nav--up">
@@ -23,21 +36,37 @@ export const SettingsLayout = ({ children }) => {
               </h1>
             </div>
             <div className="settings__sidebar__links">
-              <div className="settings__sidebar__links--item item__active">
-                <a href="" className="settings__sidebar__links--tittle">
-                  Profile
-                </a>
-              </div>
-              <div className="settings__sidebar__links--item ">
-                <a href="" className="settings__sidebar__links--tittle">
-                  Manage accounts
-                </a>
-              </div>
+              <NavLink to="/settings/profile" className="navlink">
+                <div
+                  className={
+                    pathname === "/settings/profile"
+                      ? "settings__sidebar__links--item item__active"
+                      : "settings__sidebar__links--item"
+                  }
+                >
+                  <p className="settings__sidebar__links--tittle">Profile</p>
+                </div>
+              </NavLink>
+              {role === "admin" && (
+                <NavLink to="/settings/manage" className="navlink">
+                  <div
+                    className={
+                      pathname === "/settings/manage"
+                        ? "settings__sidebar__links--item item__active"
+                        : "settings__sidebar__links--item"
+                    }
+                  >
+                    <p className="settings__sidebar__links--tittle">
+                      Manage accounts
+                    </p>
+                  </div>
+                </NavLink>
+              )}
             </div>
           </div>
           <div className="settings__sidebar__nav--down">
             <div className="settings__sidebar__nav--exit ">
-              <button className="sidebar__logout">
+              <button className="sidebar__logout" onClick={onLogout}>
                 Logout <img src="/public/icons/exit.svg" alt="exit" />
               </button>
             </div>
