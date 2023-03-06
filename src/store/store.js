970d85f6b43642339c1168133db0bc75cefd7ebf
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-03-06 01:25:45 -0500

    userSlice added

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,14 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { authSlice } from "./";
+import { usersSlice } from "./users/usersSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
+    users: usersSlice.reducer,
   },
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware({
-//       serializableCheck: false,
-//     }),    "for is working with Date"
+  //   middleware: (getDefaultMiddleware) =>
+  //     getDefaultMiddleware({
+  //       serializableCheck: false,
+  //     }),    "for is working with Date"
 });
