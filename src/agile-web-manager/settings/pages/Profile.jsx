51bcb39e54197ed8e73450a7d7f29825898aeb04
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-03-05 21:27:52 -0500

    the page was designed where the user can see your information

---
+++ src/agile-web-manager/settings/pages/Profile.jsx
@@ -0,0 +1,46 @@
+import { SettingsLayout } from "../layout/SettingsLayout";
+import "./style.css";
+
+export default function Profile() {
+  return (
+    <SettingsLayout>
+      <div className="profile">
+        <div className="profile__desc">
+          <h1 className="profile__desc--title">Profile</h1>
+          <p className="profile__desc--description">
+            view your personal information and manage your user name
+          </p>
+        </div>
+        <div className="profile__header">
+          <h2 className="profile__header--title">Profile icon</h2>
+          <div className="profile__header__image">
+            <div className="profile__header__image--top"></div>
+            <div className="profile__header__image--bottom"></div>
+            <div className="profile__header__image--icon">D</div>
+          </div>
+        </div>
+        <div className="profile__body">
+          <h2 className="profile__body--title">About you</h2>
+          <div className="profile__body__about">
+            <div className="profile__body__about--item">
+              <h3 className="about__title">full name</h3>
+              <p className="about__content">Camilo Cortes</p>
+            </div>
+            <div className="profile__body__about--item">
+              <h3 className="about__title">email</h3>
+              <p className="about__content">camilo@gmail.com</p>
+            </div>
+            <div className="profile__body__about--item">
+              <h3 className="about__title">role</h3>
+              <p className="about__content">admin</p>
+            </div>
+            <div className="profile__body__about--item">
+              <h3 className="about__title">phone</h3>
+              <p className="about__content">+573209932148</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </SettingsLayout>
+  );
+}
