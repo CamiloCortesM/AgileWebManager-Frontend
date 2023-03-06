1cb956771b5064258dbd7416d03ac9bca1cf8917
DANIELSSF <daniel.santiago730@gmail.com>
2023-03-06 12:29:31 -0500

    The functions of useTableStore were imported.

--- src/agile-web-manager/table/components/tables/Tables.jsx
+++ src/agile-web-manager/table/components/tables/Tables.jsx
@@ -1,57 +1,31 @@
 
+import { Link } from 'react-router-dom';
 import './styles.css';
 
-export const Tables = ({ toggleModal, toggleModalDelete }) => {
-
-    const handleCreateTable = () => {
-        toggleModal();
-    }
+export const Tables = ({ toggleModalDelete, user, name, id }) => {
 
     const handleDelete = () => {
-        toggleModalDelete();
+        toggleModalDelete(id);
     }
 
     return (
-        <div className="create-table">
-            <div
-                onClick={handleCreateTable}
-                className="create-table__title create-table__item"
-            >
-                <figure>
-                    <img src="public/icons/plus.svg" alt="plus.svg" />
-                </figure>
-                Create Table
-            </div>
-
+        <>
             <div className="create-table__item create-table__color">
-                <span onClick={handleDelete}>
-                    <img
-                        className="create-table__delete"
-                        src="public/icons/x.svg"
-                    ></img>
-                </span>
-                Hola Mundo!
+                {
+                    user.role == "admin" &&
+                    <span onClick={handleDelete}>
+                        <img
+                            className="create-table__delete"
+                            src="public/icons/x.svg"
+                        ></img>
+                    </span>
+                }
+                <Link to={`/table/${id}`} className="links">
+                    <p className='create-table__item__p'>
+                        {name}
+                    </p>
+                </Link>
             </div>
-
-            <div className="create-table__item create-table__color ">
-                <span onClick={handleDelete}>
-                    <img
-                        className="create-table__delete"
-                        src="public/icons/x.svg"
-                    ></img>
-                </span>
-                Sistemas Operativos
-            </div>
-
-            <div className="create-table__item create-table__color">
-                <span onClick={handleDelete}>
-                    <img
-                        className="create-table__delete"
-                        src="public/icons/x.svg"
-                    ></img>
-                </span>
-                Software I
-            </div>
-        </div>
+        </>
     );
 };
