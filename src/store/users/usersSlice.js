9df591af1cdd2c8d7557b2069a239a5dc94b807b
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-03-06 01:25:24 -0500

    reducers were created to obtain, delete, update and display the active user of all users.

---
+++ src/store/users/usersSlice.js
@@ -0,0 +1,49 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const usersSlice = createSlice({
+  name: "users",
+  initialState: {
+    errorMessage: null,
+    users: [],
+    activeUser: null,
+  },
+  reducers: {
+    onSetActiveUser: (state, { payload }) => {
+      state.activeUser = payload;
+    },
+    onDeleteActiveUser: (state, { payload }) => {
+      state.activeUser = null;
+    },
+    onLoadUsers: (state, { payload }) => {
+      state.errorMessage = false;
+      state.users = [...payload.users];
+    },
+    addNewUser: (state, { payload }) => {
+      state.users.push(payload);
+    },
+    onDeleteUser: (state, { payload }) => {
+      const index = state.users.findIndex((user) => user.id === payload);
+      if (index !== -1) {
+        state.users.splice(index, 1);
+      }
+    },
+    onUpdateUser: (state, { payload }) => {
+      console.log(payload);
+      state.users = state.users.map((user) => {
+        if (user.id === payload.id) {
+          return payload;
+        }
+        return user;
+      });
+    },
+  },
+});
+
+export const {
+  onLoadUsers,
+  addNewUser,
+  onDeleteUser,
+  onUpdateUser,
+  onSetActiveUser,
+  onDeleteActiveUser,
+} = usersSlice.actions;
