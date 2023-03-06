8f025595600e254a140ff4a613fb178967eb1e64
DANIELSSF <daniel.santiago730@gmail.com>
2023-03-06 12:26:40 -0500

    Now we handle the use of the hook useForm

--- src/agile-web-manager/table/components/modal-create/CreateTableModal.jsx
+++ src/agile-web-manager/table/components/modal-create/CreateTableModal.jsx
@@ -1,11 +1,34 @@
+import { useState } from 'react';
+import { useForm } from '../../../../hooks/useForm';
+
 import './style.css';
 
+const tableForm = {
+  name: "",
+  desc: "",
+}
 
-export const CreateTableModal = ({ toggleModal }) => {
+export const CreateTableModal = ({ toggleModal, startCreateTable }) => {
+
+  const { name, desc, onInputChange, onResetForm } = useForm(tableForm);
+
+  const [formSubmitted, setFormSubmitted] = useState(false)
+
 
   const handleCloseModal = () => {
     toggleModal();
-  }
+  };
+
+  const createTableSubmit = async(e) => {
+    e.preventDefault();
+    if (!name || !desc) {
+      setFormSubmitted(true)
+      return
+    }
+    await startCreateTable({ name: name, desc: desc });
+    onResetForm();
+    handleCloseModal();
+  };
 
 
   return (
@@ -24,34 +47,45 @@ export const CreateTableModal = ({ toggleModal }) => {
         </div>
 
         <form
-          autoComplete="off"
           className="create-table-modal__form"
+          onSubmit={createTableSubmit}
         >
           <h3 className="create-table-modal__subtitle">Table name</h3>
           <input
-            className="create-table-modal__input"
+            className={`create-table-modal__input ${
+              formSubmitted && !name &&'create-table-modal__input--invalid'
+            }`}
             placeholder="Table name"
             type="text"
+            name="name"
+            value={name}
+            onChange={onInputChange}
           />
           <h3 className="create-table-modal__subtitle">Description</h3>
           <textarea
-            className="create-table-modal__input create-table-modal__input__textarea"
+            className={`create-table-modal__input ${
+              formSubmitted && !desc &&'create-table-modal__input--invalid'
+            }`}
             placeholder="Description"
             type="text"
+            name="desc"
+            value={desc}
+            onChange={onInputChange}
           />
 
           <div className="create-table-modal__buttons">
             <button
               className="create-table-modal__button create-table-modal__button__close"
               onClick={handleCloseModal}
+              type="button"
             >
               Cancel</button>
-            <button className="create-table-modal__button create-table-modal__button__create">Create</button>
+            <button type="submit" className="create-table-modal__button create-table-modal__button__create">
+              Create</button>
           </div>
         </form>
-      </div>
+      </div >
 
-
-    </div>
+    </div >
   )
 }
