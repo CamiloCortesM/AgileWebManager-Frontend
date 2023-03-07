30cf8985cce1d7d2a3946fa278ac2a15615b703c
DANIELSSF <daniel.santiago730@gmail.com>
2023-03-06 20:33:44 -0500

    Edit modal has been created and delete modal is now disable

--- src/agile-web-manager/table/components/tables/Tables.jsx
+++ src/agile-web-manager/table/components/tables/Tables.jsx
@@ -1,25 +1,56 @@
 
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 import './styles.css';
 
-export const Tables = ({ toggleModalDelete, user, name, id }) => {
+export const Tables = ({ table, startDeleteTable, startSetActiveTable, user, toggleModalEdit }) => {
+    const { name, id } = table;
 
-    const handleDelete = () => {
-        toggleModalDelete(id);
-    }
+    const onDelete = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                startDeleteTable({ id });
+            }
+        });
+    };
+
+    const handleActiveTable = (table) => {
+        startSetActiveTable(table);
+        toggleModalEdit();
+    };
+
 
     return (
         <>
             <div className="create-table__item create-table__color">
                 {
                     user.role == "admin" &&
-                    <span onClick={handleDelete}>
-                        <img
-                            className="create-table__delete"
-                            src="public/icons/x.svg"
-                        ></img>
-                    </span>
+                    <>
+                        <span onClick={() => onDelete(id)}>
+                            <img
+                                className="create-table__delete"
+                                src="public/icons/x.svg"
+                            ></img>
+                        </span>
+
+                        <span onClick={() => handleActiveTable(table)}>
+                            <img
+                                className="create-table__edit"
+                                src="public/icons/edit.svg"
+                            ></img>
+                        </span>
+                    </>
                 }
+
+
                 <Link to={`/table/${id}`} className="links">
                     <p className='create-table__item__p'>
                         {name}
