d69dcc38062cfde40544c4157e9c6594a732654b
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-03-25 17:12:59 -0500

    functionality to open modal in todos Done

--- src/agile-web-manager/todos/components/todo-done/Done.jsx
+++ src/agile-web-manager/todos/components/todo-done/Done.jsx
@@ -1,3 +1,4 @@
+import { useCommentStore } from "../../../../hooks/useCommentStore";
 import { DeleteTodo } from "../index";
 import "../style.css";
 
@@ -13,9 +14,16 @@ export const Done = ({
   startSetActiveTodo,
   toggleModalEdit,
 }) => {
+  const { startLoadComments } = useCommentStore();
   const handleGetInfoTodo = () => {
     toggleModalEdit(true);
-    startSetActiveTodo({ name: name, status: status, id: id, desc: desc });
+    startSetActiveTodo({ name, status, id, desc });
+  };
+
+  const openModal = () => {
+    startSetActiveTodo({ name, status, id, desc });
+    startLoadComments(comments);
+    handleModal();
   };
 
   return (
@@ -30,7 +38,7 @@ export const Done = ({
               src="/public/icons/text.svg"
               className="todo__tarjet__text"
               alt="text.svg"
-              onClick={handleModal}
+              onClick={openModal}
             ></img>
             <div className="todo__tarjet__comments">
               <img
