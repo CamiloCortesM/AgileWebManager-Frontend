b74eef2ec502cfd9dcf6d7157b5af7ce58165c30
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-03-25 17:13:24 -0500

    functionality to open modal in todo in progress

--- src/agile-web-manager/todos/components/todo-in-progress/InProgress.jsx
+++ src/agile-web-manager/todos/components/todo-in-progress/InProgress.jsx
@@ -1,20 +1,35 @@
-import { DeleteTodo } from '../index';
-import '../style.css'
+import { useCommentStore } from "../../../../hooks/useCommentStore";
+import { DeleteTodo } from "../index";
+import "../style.css";
 
-export const InProgress = ({ name, status, comments, id, desc, user, startDeleteTodo, startSetActiveTodo, toggleModalEdit,handleModal }) => {
-
+export const InProgress = ({
+  name,
+  status,
+  comments,
+  id,
+  desc,
+  user,
+  startDeleteTodo,
+  startSetActiveTodo,
+  toggleModalEdit,
+  handleModal,
+}) => {
+  const { startLoadComments } = useCommentStore();
   const handleGetInfoTodo = () => {
     toggleModalEdit(true);
-    startSetActiveTodo({ name: name, status: status, id: id, desc: desc });
+    startSetActiveTodo({ name, status, id, desc });
+  };
+
+  const openModal = () => {
+    startSetActiveTodo({ name, status, id, desc });
+    startLoadComments(comments);
+    handleModal();
   };
 
   return (
     <>
-
-      {
-        (status == "progress") &&
+      {status == "progress" && (
         <li className="todo__tarjet">
-
           <span onClick={user !== "readOnly" && handleGetInfoTodo}>
             <h3 className="todo__tarjet__subtitle">{name}</h3>
           </span>
@@ -24,7 +39,7 @@ export const InProgress = ({ name, status, comments, id, desc, user, startDelete
               src="/public/icons/text.svg"
               className="todo__tarjet__text"
               alt="text.svg"
-              onClick={handleModal}
+              onClick={openModal}
             ></img>
             <div className="todo__tarjet__comments">
               <img
@@ -36,17 +51,12 @@ export const InProgress = ({ name, status, comments, id, desc, user, startDelete
             </div>
             <div className="todo__tarjet__space"></div>
 
-            {
-              user !== 'readOnly' &&
-              <DeleteTodo
-                id={id}
-                startDeleteTodo={startDeleteTodo}
-              />
-            }
+            {user !== "readOnly" && (
+              <DeleteTodo id={id} startDeleteTodo={startDeleteTodo} />
+            )}
           </div>
         </li>
-      }
-
+      )}
     </>
-  )
-}
+  );
+};
