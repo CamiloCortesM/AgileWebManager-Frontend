3150f4a287767606beecb50eb25e0b26e0fd65b3
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-03-25 00:05:30 -0500

    added the option to open the modal in the TODOS section facts

--- src/agile-web-manager/todos/components/todo-done/Done.jsx
+++ src/agile-web-manager/todos/components/todo-done/Done.jsx
@@ -1,46 +1,52 @@
-import { DeleteTodo } from '../index';
-import '../style.css';
+import { DeleteTodo } from "../index";
+import "../style.css";
 
-export const Done = ({ name, status, user, comments, id, desc, startDeleteTodo, startSetActiveTodo, toggleModalEdit }) => {
+export const Done = ({
+  handleModal,
+  name,
+  status,
+  user,
+  comments,
+  id,
+  desc,
+  startDeleteTodo,
+  startSetActiveTodo,
+  toggleModalEdit,
+}) => {
+  const handleGetInfoTodo = () => {
+    toggleModalEdit(true);
+    startSetActiveTodo({ name: name, status: status, id: id, desc: desc });
+  };
 
-    const handleGetInfoTodo = () => {
-        toggleModalEdit(true);
-        startSetActiveTodo({ name: name, status: status, id: id, desc: desc });
-    };
-
-    return (
-        <>
-            {
-                (status == "done") &&
-                <li className="todo__tarjet">
-                    <span onClick={user !== "readOnly" && handleGetInfoTodo}>
-                        <h3 className="todo__tarjet__subtitle">{name}</h3>
-                    </span>
-                    <div className="todo__tarjet__icons">
-                        <img
-                            src="/public/icons/text.svg"
-                            className="todo__tarjet__text"
-                            alt="text.svg"
-                        ></img>
-                        <div className="todo__tarjet__comments">
-                            <img
-                                src="/public/icons/message.svg"
-                                className="todo__tarjet__message"
-                                alt="message.svg"
-                            ></img>
-                            {comments.length}
-                        </div>
-                        <div className="todo__tarjet__space"></div>
-                        {
-                            user !== "readOnly" &&
-                            <DeleteTodo
-                                id={id}
-                                startDeleteTodo={startDeleteTodo}
-                            />
-                        }
-                    </div>
-                </li>
-            }
-        </>
-    )
-}
+  return (
+    <>
+      {status == "done" && (
+        <li className="todo__tarjet">
+          <span onClick={user !== "readOnly" && handleGetInfoTodo}>
+            <h3 className="todo__tarjet__subtitle">{name}</h3>
+          </span>
+          <div className="todo__tarjet__icons">
+            <img
+              src="/public/icons/text.svg"
+              className="todo__tarjet__text"
+              alt="text.svg"
+              onClick={handleModal}
+            ></img>
+            <div className="todo__tarjet__comments">
+              <img
+                src="/public/icons/message.svg"
+                className="todo__tarjet__message"
+                alt="message.svg"
+              ></img>
+              {comments.length}
+            </div>
+            <div className="todo__tarjet__space"></div>
+            {user !== "readOnly" && (
+              <DeleteTodo id={id} startDeleteTodo={startDeleteTodo} />
+            )}
+          </div>
+        </li>
+      )}
+    </>
+  );
+};
