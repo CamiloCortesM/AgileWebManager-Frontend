90b46d3ed306cd5d2110f95313a1bcdae3125a27
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-03-25 00:05:58 -0500

    functionality was created to open and close the modal

--- src/agile-web-manager/todos/pages/TodoPages.jsx
+++ src/agile-web-manager/todos/pages/TodoPages.jsx
@@ -1,16 +1,16 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useTableStore } from '../../../hooks/useTableStore';
-import { useTodoStore } from '../../../hooks/useTodoStore';
-import { NavbarLayout } from '../../layout/NavbarLayout';
-import { AddTarjetCreate } from '../components/add-tarjet/AddTarjetCreate';
-import { ModalTodo } from '../components/modal-todo/ModalTodo';
-import { Done, InProgress, NotStarted } from '../index';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useTableStore } from "../../../hooks/useTableStore";
+import { useTodoStore } from "../../../hooks/useTodoStore";
+import { CommentView } from "../../comments/view/CommentView";
+import { NavbarLayout } from "../../layout/NavbarLayout";
+import { AddTarjetCreate } from "../components/add-tarjet/AddTarjetCreate";
+import { ModalTodo } from "../components/modal-todo/ModalTodo";
+import { Done, InProgress, NotStarted } from "../index";
 
-import './style.css';
+import "./style.css";
 
 export const TodoPages = () => {
-
   const {
     tables,
     todos,
@@ -26,18 +26,21 @@ export const TodoPages = () => {
   const { startGetTables } = useTableStore();
   const location = useParams();
   const [showModalEditTodo, setShowModalEditTodo] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
+  const handleModal = () => {
+    setShowModal(!showModal);
+  };
 
   useEffect(() => {
     startGetTables();
     startLoadTodos();
   }, []);
 
-
   const toggleModalEdit = () => {
     setShowModalEditTodo(!showModalEditTodo);
   };
-  const { id } = useParams()
+  const { id } = useParams();
 
   const getTable = tables.find((table) => table.id === location.id);
   const tableTodos = getTable ? getTable.todos : [];
@@ -45,104 +48,91 @@ export const TodoPages = () => {
 
   return (
     <NavbarLayout>
-
       <div className="todo-pages">
         <h2 className="todo-pages__title">{getTable?.name}</h2>
         <p className="todo-pages__title">{getTable?.desc}</p>
         <div className="todo-pages__container">
-
-          <div className="todo" >
+          <div className="todo">
             <h2 className="todo__title">Not Started</h2>
             <ul className="todo__tarjet__container">
-              {
-                todosInTable.map(todo => (
-                  <NotStarted
-                    key={todo.id}
-                    {...todo}
-                    startDeleteTodo={startDeleteTodo}
-                    startSetActiveTodo={startSetActiveTodo}
-                    toggleModalEdit={toggleModalEdit}
-                    user={user.role}
-                  />
-                ))
-              }
+              {todosInTable.map((todo) => (
+                <NotStarted
+                  key={todo.id}
+                  {...todo}
+                  startDeleteTodo={startDeleteTodo}
+                  startSetActiveTodo={startSetActiveTodo}
+                  toggleModalEdit={toggleModalEdit}
+                  handleModal={handleModal}
+                  user={user.role}
+                />
+              ))}
             </ul>
-            {
-              user.role !== "readOnly" &&
+            {user.role !== "readOnly" && (
               <AddTarjetCreate
                 status="start"
                 startSaveTodos={startSaveTodos}
                 getTable={getTable?.id}
               />
-            }
+            )}
           </div>
 
           <div className="todo">
             <h2 className="todo__title">In Progress</h2>
             <ul className="todo__tarjet__container">
-
-              {
-                todosInTable.map(todo => (
-                  <InProgress
-                    key={todo.id}
-                    {...todo}
-                    startDeleteTodo={startDeleteTodo}
-                    startSetActiveTodo={startSetActiveTodo}
-                    toggleModalEdit={toggleModalEdit}
-                    user={user.role}
-                  />
-
-                ))
-              }
+              {todosInTable.map((todo) => (
+                <InProgress
+                  key={todo.id}
+                  {...todo}
+                  startDeleteTodo={startDeleteTodo}
+                  startSetActiveTodo={startSetActiveTodo}
+                  toggleModalEdit={toggleModalEdit}
+                  handleModal={handleModal}
+                  user={user.role}
+                />
+              ))}
             </ul>
-            {
-              user.role !== "readOnly" &&
+            {user.role !== "readOnly" && (
               <AddTarjetCreate
                 status="progress"
                 startSaveTodos={startSaveTodos}
                 getTable={getTable?.id}
               />
-            }
+            )}
           </div>
 
           <div className="todo">
             <h2 className="todo__title">Done</h2>
             <ul className="todo__tarjet__container">
-              {
-                todosInTable.map(todo => (
-                  <Done
-                    key={todo.id}
-                    {...todo}
-                    startDeleteTodo={startDeleteTodo}
-                    startSetActiveTodo={startSetActiveTodo}
-                    toggleModalEdit={toggleModalEdit}
-                    user={user.role}
-                  />
-                ))
-              }
+              {todosInTable.map((todo) => (
+                <Done
+                  key={todo.id}
+                  {...todo}
+                  startDeleteTodo={startDeleteTodo}
+                  startSetActiveTodo={startSetActiveTodo}
+                  toggleModalEdit={toggleModalEdit}
+                  handleModal={handleModal}
+                  user={user.role}
+                />
+              ))}
             </ul>
-            {
-              user.role !== "readOnly" &&
+            {user.role !== "readOnly" && (
               <AddTarjetCreate
                 status="done"
                 startSaveTodos={startSaveTodos}
                 getTable={getTable?.id}
               />
-            }
+            )}
           </div>
-
         </div>
-
       </div>
-      {
-        showModalEditTodo &&
+      {showModalEditTodo && (
         <ModalTodo
           todoActive={todoActive}
           toggleModalEdit={toggleModalEdit}
           startUpdateTodo={startUpdateTodo}
         />
-      }
-
+      )}
+      <CommentView show={showModal} handleModal={handleModal} />
     </NavbarLayout>
-  )
-}
+  );
+};
