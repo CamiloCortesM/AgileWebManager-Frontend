e566d5ad4429984214859604cc1d6e74a7714d5b
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-03-25 17:11:06 -0500

    added all functionality and alerts for the creation and viewing of comments

--- src/agile-web-manager/comments/view/CommentView.jsx
+++ src/agile-web-manager/comments/view/CommentView.jsx
@@ -1,4 +1,10 @@
+import Swal from "sweetalert2";
+import { getLetter } from "../../../helpers/getLetter";
+import { getDate } from "../../../helpers/getDate";
+import { useAuthStore } from "../../../hooks/useAuthStore";
+import { useCommentStore } from "../../../hooks/useCommentStore";
 import { useForm } from "../../../hooks/useForm";
+import { useTodoStore } from "../../../hooks/useTodoStore";
 import "./styles.css";
 
 const createFields = {
@@ -6,28 +12,76 @@ const createFields = {
 };
 
 export const CommentView = ({ show, handleModal }) => {
-  const { onInputChange, comment, onResetForm } = useForm(createFields);
+  const { onInputChange, comment } = useForm(createFields);
+
+  const { todoActive } = useTodoStore();
+  const { comments, startSaveComment, startLogoutComment, startDeleteComment } =
+    useCommentStore();
+  const { user } = useAuthStore();
+
+  const closeModal = () => {
+    handleModal();
+    startLogoutComment();
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log("Hola");
+    startSaveComment({ comment, todoId: todoActive.id });
+    Swal.fire("Good job!", "Comment Created", "success");
   };
+
+  const DeleteComment = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        startDeleteComment({ id });
+      }
+    });
+  };
+
   return (
-    <div className={`modal ${show ? "show" : ""}`} onClick={handleModal}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <button className="close-button" onClick={handleModal}>
+    <div className={`modal ${show ? "show" : ""}`} onClick={closeModal}>
+      <div className="modal-todo" onClick={(e) => e.stopPropagation()}>
+        <button className="todo--button" onClick={closeModal}>
           X
         </button>
-        <div className="todo__header_">
-          <h2 className="todo__header_--title">TODO</h2>
-          <p className="todo__header_--desc">desc todo</p>
+        <div className="todo__header">
+          <div className="todo__header__tittle">
+            <img
+              src="/public/icons/board-presentation-education-svgrepo-com.svg"
+              className="todo__header--icon"
+              alt="icon-todo"
+            ></img>
+            <h2 className="todo__header--tittle">{todoActive?.name}</h2>
+            <p className="todo__header--status">
+              <span> {todoActive?.status} </span>
+            </p>
+          </div>
+          <p className="todo__header--desc">{todoActive?.desc}</p>
         </div>
         <div className="todo__activity">
-          <h3 className="todo__activity--title">Activity</h3>
+          <div className="todo__activity__header">
+            <img
+              src="/public/icons/bullets-svgrepo-com.svg"
+              className="activity--icon"
+              alt="icon-todo"
+            ></img>
+            <h3 className="todo__activity--title">Activity</h3>
+          </div>
+
           <div className="todo__comment">
-            <form onSubmit={onSubmit}>
+            <form className="form__todo" onSubmit={onSubmit}>
               <div className="form__modal__image">
-                <p className="form__modal__image--icon">C</p>
+                <p className="form__modal__image--icon">
+                  {getLetter(user.name)}
+                </p>
               </div>
               <div className="form__modal__comment">
                 <input
@@ -40,23 +94,52 @@ export const CommentView = ({ show, handleModal }) => {
                   id="comment"
                   required
                 />
-                <button type="submit" className="modal--button">
-                  Save
-                </button>
+                {comment != "" && (
+                  <button type="submit" className="button--send">
+                    <img
+                      type="submit"
+                      className="icon--send"
+                      src="/public/icons/send-2-svgrepo-com.svg"
+                      alt="send"
+                    />
+                  </button>
+                )}
               </div>
             </form>
           </div>
           <div className="todo__activity__content">
-            <div className="comment__icon"></div>
-            <div className="comment__content">
-              <div className="comment__content__header">
-                <h3 className="comment--autor">Nombre Apellido</h3>
-                <p className="comment--date">fecha</p>
+            {comments.map((comment) => (
+              <div key={comment._id} className="todo__activity__comment">
+                <div className="comment__icon">
+                  <p className="comment__icon--icon">
+                    {getLetter(comment.creator.name)}
+                  </p>
+                </div>
+                <div className="comment__content">
+                  <div className="comment__content__header">
+                    <h4 className="comment--autor">{comment.creator.name}</h4>
+                    <p className="comment--date">{getDate(comment.date)}</p>
+                  </div>
+                  <div className="comment__content__body">
+                    <p className="comment--text">{comment.comment}</p>
+                  </div>
+                  {user.uid === comment.creator._id && (
+                    <div className="comment__actions">
+                      <ul className="comment_list">
+                        <li className="comment__list__item">
+                          <p
+                            className="list__item--action"
+                            onClick={() => DeleteComment(comment._id)}
+                          >
+                            Eliminar
+                          </p>
+                        </li>
+                      </ul>
+                    </div>
+                  )}
+                </div>
               </div>
-              <div className="comment__content__body">
-                <p className="comment--text">Hola</p>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
