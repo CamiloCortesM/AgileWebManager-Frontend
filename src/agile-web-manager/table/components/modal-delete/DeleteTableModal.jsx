6f62ddd37fb8c88c707889075d031c59f9accdce
DANIELSSF <daniel.santiago730@gmail.com>
2023-03-27 16:10:43 -0500

    Class names have been changed

--- src/agile-web-manager/table/components/modal-delete/DeleteTableModal.jsx
+++ src/agile-web-manager/table/components/modal-delete/DeleteTableModal.jsx
@@ -1,4 +1,3 @@
-import './style.css';
 
 export const DeleteTableModal = ({ toggleModalDelete, idDelete, startDeleteTable }) => {
 
@@ -12,27 +11,38 @@ export const DeleteTableModal = ({ toggleModalDelete, idDelete, startDeleteTable
     }
 
     return (
-        <div className="delete-table-modal">
-            <div className="delete-table-modal__container">
-                <h3 className="delete-table-modal__title">Delete Table</h3>
-                <span onClick={handleDelete}>
-                    <img
-                        className="delete-table-modal__delete"
-                        src="public/icons/x.svg"
-                    ></img>
+        <div className = "delete-table-modal">
+            <div className = "delete-table-modal__container">
+                <h3 className = "delete-table-modal__title">Delete Table</h3>
+                <span onClick = {handleDelete}>
+
+                    <figure>
+                        <img
+                            className = "delete-table-modal__delete"
+                            src       = "public/icons/x.svg"
+                        />
+                    </figure>
+
                 </span>
-                <p className="delete-table-modal__p">Are you sure you want to delete your table?</p>
-                <div className="delete-table-modal__buttons">
+
+                <p className = "delete-table-modal__p">Are you sure you want to delete your table?</p>
+
+                <div className = "delete-table-modal__buttons">
                     <button
-                        type="button"
-                        onClick={handleDelete}
-                        className="delete-table-modal__button delete-table-modal__button__cancel"
-                    >Cancel</button>
+                        type      = "button"
+                        onClick   = {handleDelete}
+                        className = "delete-table-modal__button delete-table-modal__button__cancel"
+                    >
+                        Cancel
+                    </button>
+
                     <button
-                        type="button"
-                        onClick={() => deleteTable(idDelete)}
-                        className="delete-table-modal__button delete-table-modal__button__delete"
-                    >Delete</button>
+                        type      = "button"
+                        onClick   = {() => deleteTable(idDelete)}
+                        className = "delete-table-modal__button delete-table-modal__button__delete"
+                    >
+                        Delete
+                    </button>
                 </div>
             </div>
         </div>
