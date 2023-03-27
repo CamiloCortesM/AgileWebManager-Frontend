8d15bf36e17bba525003ae02f5c3cccb308f0365
DANIELSSF <daniel.santiago730@gmail.com>
2023-03-27 17:09:35 -0500

    Props and arguments updated

--- src/agile-web-manager/todos/components/todo-not-started/NotStarted.jsx
+++ src/agile-web-manager/todos/components/todo-not-started/NotStarted.jsx
@@ -1,49 +1,76 @@
-import { ChangeTodo, DeleteTodo } from '../index';
+import { DeleteTodo } from '../index';
+import PropTypes from 'prop-types';
 
 import '../style.css';
 
-export const NotStarted = ({ name, status, user, comments, id, desc, startDeleteTodo, startSetActiveTodo, toggleModalEdit }) => {
+export const NotStarted = ({ 
+    comments, 
+    desc, 
+    id, 
+    name, 
+    startDeleteTodo, 
+    startSetActiveTodo, 
+    status, 
+    toggleModalEdit,
+    user, 
+  }) => {
 
     const handleGetInfoTodo = () => {
         toggleModalEdit(true);
-        startSetActiveTodo({ name: name, status: status, id: id, desc: desc });
+        startSetActiveTodo({ name, status, id, desc });
     };
 
     return (
         <>
             {
                 (status == "start") &&
-                <li className="todo__tarjet">
-                    <span onClick={user !== "readOnly" && handleGetInfoTodo}>
-                        <h3 className="todo__tarjet__subtitle">{name}</h3>
+                <li className = "todo__tarjet">
+
+                    <span onClick = {user !== "readOnly" && handleGetInfoTodo}>
+                        <h3 className = "todo__tarjet__subtitle">{name}</h3>
                     </span>
-                    <div className="todo__tarjet__icons">
+
+                    <div className = "todo__tarjet__icons">
+                        
                         <img
-                            src="/public/icons/text.svg"
-                            className="todo__tarjet__text"
-                            alt="text.svg"
-                        ></img>
-                        <div className="todo__tarjet__comments">
+                            src       = "/public/icons/text.svg"
+                            className = "todo__tarjet__text"
+                            alt       = "text.svg"
+                        />
+
+                        <div className = "todo__tarjet__comments">
+
                             <img
-                                src="/public/icons/message.svg"
-                                className="todo__tarjet__message"
-                                alt="message.svg"
-                            ></img>
+                                src       = "/public/icons/message.svg"
+                                className = "todo__tarjet__message"
+                                alt       = "message.svg"
+                            />
                             {comments.length}
                         </div>
-                        <div className="todo__tarjet__space"></div>
+
+                        <div className = "todo__tarjet__space"></div>
                         {
                             user !== "readOnly" &&
                             <DeleteTodo
-                                id={id}
-                                startDeleteTodo={startDeleteTodo}
+                                id = {id}
+                                startDeleteTodo = {startDeleteTodo}
                             />
                         }
                     </div>
                 </li>
-
             }
-
         </>
     )
+}
+
+
+NotStarted.propTypes = {
+    desc               : PropTypes.string,
+    id                 : PropTypes.string.isRequired,
+    name               : PropTypes.string.isRequired,
+    startDeleteTodo    : PropTypes.func.isRequired,
+    startSetActiveTodo : PropTypes.func.isRequired,
+    status             : PropTypes.string.isRequired,
+    toggleModalEdit    : PropTypes.func.isRequired,
+    user               : PropTypes.string.isRequired,
 }
