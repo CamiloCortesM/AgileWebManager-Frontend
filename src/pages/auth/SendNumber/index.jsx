022694e2d9fe5b6fc0c689f4aacc0b611b462ccd
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-06-19 16:28:31 -0500

    refactoring into smaller components

--- src/pages/auth/SendNumber/index.jsx
+++ src/pages/auth/SendNumber/index.jsx
@@ -2,27 +2,19 @@ import { useEffect, useState } from 'react';
 import Swal from 'sweetalert2';
 
 import { useAuthStore } from '../../../hooks/useAuthStore';
+import { NumberHeader } from '../../../components/NumberHeader';
+import { NumberForm } from '../../../components/NumberForm';
 
 import './styles.css';
-import '../shares-styles/auth-form.css';
 
 export const SendNumberPage = () => {
-  const [number, setNumber] = useState('');
+  const [phone, setPhone] = useState('');
 
   const { startSendNumber, errorMessage } = useAuthStore();
 
-  const onInputChange = ({ target }) => {
-    const value = target.value;
-    if (value.length > 10) return;
-    const isValid = /^\d*$/.test(value);
-    if (!isValid) return;
-    setNumber(target.value);
-  };
-
-  const onSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const numberPhone = '+57' + number;
-    startSendNumber({ phone: numberPhone });
+    startSendNumber({ phone });
   };
 
   useEffect(() => {
@@ -30,6 +22,7 @@ export const SendNumberPage = () => {
       Swal.fire('Error in the phone number', errorMessage, 'error');
     }
   }, [errorMessage]);
+
   return (
     <div className="container__number">
       <img
@@ -38,45 +31,13 @@ export const SendNumberPage = () => {
         alt="wave"
       />
       <div className="number">
-        <div className="number__header">
-          <figure className="number__header__image">
-            <img
-              className="number__image__phone"
-              src="/public/icons/smartphone-telephone-svgrepo-com.svg"
-              alt="smartphone"
-            />
-          </figure>
-          <h2 className="number__header__title">Verify Your Number</h2>
-          <p className="number__header__description">
-            Please enter your Phone Number
-          </p>
-        </div>
+        <NumberHeader />
         <div className="number__body">
-          <form className="number__body__form" onSubmit={onSubmit}>
-            <div className="number__body__phone">
-              <div className="number__body__phone--code">
-                <img
-                  className="number__body__image--flag"
-                  src="/public/icons/flag-for-flag-colombia-svgrepo-com.svg"
-                  alt="flag"
-                />
-                <p className="number__body__image--code">+57</p>
-              </div>
-              <input
-                placeholder="- - - - - - - - - - -"
-                type="text"
-                name="number"
-                value={number}
-                onChange={onInputChange}
-                className="number__input__phone input"
-              />
-            </div>
-            <div className="number__footer">
-              <button className="number__footer__button button" type="submit">
-                Send
-              </button>
-            </div>
-          </form>
+          <NumberForm
+            onSubmit={handleSubmit}
+            phone={phone}
+            setPhone={setPhone}
+          />
         </div>
       </div>
     </div>
